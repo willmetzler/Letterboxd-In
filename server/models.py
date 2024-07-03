391c6155e449b67914b80a7dda10a3e36ad3ec7f
Will Metzler <metzlerwill@gmail.com>
2024-07-03 13:05:49 -0400

    add models

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 from config import db, bcrypt
@@ -13,6 +13,12 @@ class User(db.Model, SerializerMixin):
     last_name = db.Column(db.String, nullable=False)
     _hashed_password = db.Column(db.String)
 
+    # Relationships
+    movies = relationship('Movie', backref='user')
+    watchlist = relationship('Watchlist', backref='user')
+
+    serialize_rules = ('-movies.user', '-watchlist.user')
+
 
 class Movie(db.Model, SerializerMixin):
     __tablename__ ='movies_table'
@@ -22,8 +28,11 @@ class Movie(db.Model, SerializerMixin):
     director = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     image = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Integer)
+    rating = db.Column(db.Float)
     review = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users_table.id'))
+
+    serialize_rules = ('-user.movies',)
 
 
 class Watchlist(db.Model, SerializerMixin):
@@ -34,3 +43,12 @@ class Watchlist(db.Model, SerializerMixin):
     director = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     image = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users_table.id'))
+
+    serialize_rules = ('-user.watchlist',)
+
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if rating is not None and (rating < 0 or rating > 10):
+            raise ValueError("Rating must be between 0 and 10")
+        return rating
